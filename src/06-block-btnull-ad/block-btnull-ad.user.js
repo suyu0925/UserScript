cf8fc0514531c62c2cca1cf761826c270a2d2a57
suyu <13936562@qq.com>
2023-03-18 23:52:05 +0800

    update block-btnull-ad.user

--- src/06-block-btnull-ad/block-btnull-ad.user.js
+++ src/06-block-btnull-ad/block-btnull-ad.user.js
@@ -3,9 +3,11 @@
 // @namespace       https://github.com/suyu0925/UserScript
 // @description     Block BTNull Ad.
 // @run-at          document-end
-// @include         http*://www.btnull.org/*
-// @include         http*://btnull.org/*
-// @version         0.0.1
+// @match           http*://www.btnull.org/*
+// @match           http*://btnull.org/*
+// @match           http*://www.pkmp4.com/*
+// @match           http*://pkmp4.com/*
+// @version         0.0.2
 // ==/UserScript==
 
 const hideAd = () => {
@@ -16,6 +18,10 @@ const hideAd = () => {
   // right
   const ad_right = document.getElementById('HMcoupletDivright')
   ad_right.style.display = 'none'
+
+  // rich box
+  const ad_rich_box = document.getElementById('HMRichBox')
+  ad_rich_box.style.display = 'none'
 }
 
 const onDocumentChange = () => {
